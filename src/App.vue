3065c9722b27d0922b2ea236c6c50c4e67b69997
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-14 14:40:44 -0400

    Starting

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue';
+
+  /* Para deixar o atributo dinâmico e reativo, precisar usar o ref do Vue 
+   * Somente para valores primitivos: números, strings, etc...
+  */
+  let count = ref(0);
+
+  function increment() {
+    count.value++;
+  }
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="increment()">Incrementar</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
