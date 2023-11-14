e836a37ff097ee5b5209db66a547974630f56279
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-14 15:35:32 -0400

    Tabalhando com OptionsAPI | Reatividade com ref e reactive | Trabalhando com props e Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <script setup>
-  import { ref } from 'vue';
+  import { reactive } from 'vue';
+  import Progress from './components/Progress.vue';
 
-  /* Para deixar o atributo dinâmico e reativo, precisar usar o ref do Vue 
-   * Somente para valores primitivos: números, strings, etc...
-  */
-  let count = ref(0);
+  // reactive usado, convencionalmente, para objetos reativos e dinâmicos
+  let courses = reactive([
+    {
+      title: "JavaScript",
+      description: "Curso básico de JavaScript",
+      done: true
+    },
+    {
+      title: "React",
+      description: "Curso básico de React",
+      done: false
+    },
+    {
+      title: "Vue",
+      description: "Curso básico de Vue",
+      done: true
+    }
+  ]);
 
-  function increment() {
-    count.value++;
+  let newCourse = {done:false};
+
+  function addCourse() {
+    courses.push(newCourse);
+    newCourse = {done:false};
   }
 
 </script>
 
 <template>
   <div>
-    <h1>{{ count }}</h1>
-    <button @click="increment()">Incrementar</button>
+    <h1>Lista de Cursos</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.title">
+        {{ course.title }}
+        <ul>
+          <li>{{ course.description }}</li>
+        </ul>
+      </li>
+    </ul>
+
+    <br />
+
+    <p>Adicionar curso:</p>
+    <input v-model="newCourse.title" type="text" placeholder="Nome do curso..."> <br />
+    <input v-model="newCourse.description" type="text" placeholder="Descrição do curso..."> <br />
+    <button @click="addCourse()">Adicionar</button>
+
+    <Progress :courses="courses" />
+
   </div>
 </template>
 
